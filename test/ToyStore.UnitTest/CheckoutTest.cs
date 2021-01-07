47f661165130dcca0a1ed3cfd0c9c16326c8f622
Hian Castilho <hian_castilho@poli.ufrj.br>
2021-01-07 20:39:22 -0300

    :red_circle: Create first unit test.

---
+++ test/ToyStore.UnitTest/CheckoutTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ToyStore.UnitTest
+{
+    public class CheckoutTest
+    {
+        [Fact]
+        public void AddItem_Should_Increase_Total_Chart_Price()
+        {
+            var checkout = new Checkout();
+            checkout.AddItem(new Toy(5));
+            Assert.Equal(5, checkout.TotalPrice);
+        }
+    }
+}
