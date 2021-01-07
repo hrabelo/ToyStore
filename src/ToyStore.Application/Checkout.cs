ec5e5228fdda73577aa99372d1fae0a2d1bad989
Hian Castilho <hian_castilho@poli.ufrj.br>
2021-01-07 20:48:53 -0300

    :white_check_mark: Make the first test pass

---
+++ src/ToyStore.Application/Checkout.cs
@@ -0,0 +1,12 @@
+﻿namespace ToyStore.Application
+{
+    public class Checkout
+    {
+        public decimal TotalPrice { get; set; }
+
+        public void AddItem(decimal price)
+        {
+            TotalPrice += price;
+        }
+    }
+}
