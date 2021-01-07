f353b5766c45d8fd1695dced8d5cb0c8cf682954
Hian Castilho <hian_castilho@poli.ufrj.br>
2021-01-07 20:51:50 -0300

    :red_circle: Create second unit test failing

--- test/ToyStore.UnitTest/CheckoutTest.cs
+++ test/ToyStore.UnitTest/CheckoutTest.cs
@@ -12,5 +12,13 @@ namespace ToyStore.UnitTest
             checkout.AddItem(5);
             Assert.Equal(5, checkout.TotalPrice);
         }
+
+        [Fact]
+        public void AddItem_5_Times_Same_Product_Should_Give_10_Percent_Discount()
+        {
+            var checkout = new Checkout();
+            checkout.AddItem("Doll", 5, 10);
+            Assert.Equal(45, checkout.TotalPrice);
+        }
     }
 }
